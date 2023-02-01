ceb899e40a0d548e0e4bfb27f56e4a43c47d2933
Luis Rondon <leduardo.rondon@gmail.com>
2023-01-31 22:23:42 -0400

    update packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
