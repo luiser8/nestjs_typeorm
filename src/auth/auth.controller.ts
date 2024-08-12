3a995493415c905a712118d9471c6d438619d370
Luis E. Rondon <lrondono@achs.cl>
2024-08-11 23:18:24 -0400

    update packages

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,66 +1,76 @@
 import {
-    Body,
-    Controller,
-    Get,
-    Post,
-    HttpCode,
-    HttpStatus,
-    Query,
-    UseGuards,
+  Body,
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
 import { EmailService } from '../email/email.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { RefreshTokenDto, UserLoginDto, UserLoginDtoAuth, UserLoginDtoError, UserUpdateTokenDto } from 'src/users/dto/userLoginDto';
+import {
+  RefreshTokenDto,
+  UserLoginDto,
+  UserLoginDtoAuth,
+  UserLoginDtoError,
+  UserUpdateTokenDto,
+} from 'src/users/dto/userLoginDto';
 import { AuthGuard } from './auth.guard';
 
 @ApiTags('Auth')
 @Controller({
-    path: 'auth',
-    version: '1',
+  path: 'auth',
+  version: '1',
 })
 export class AuthController {
-    constructor (
-        private authService: AuthService,
-        private emailService: EmailService,
-    ) { }
+  constructor(
+    private authService: AuthService,
+    private emailService: EmailService,
+  ) {}
 
-    @ApiOperation({ summary: 'User authentication' })
-    @ApiResponse({
-        status: HttpStatus.OK,
-        description: "Success",
-        type: UserLoginDtoAuth
-    })
-    @ApiResponse({
-        status: HttpStatus.UNAUTHORIZED,
-        description: "Error",
-        type: UserLoginDtoError
-    })
-    @Post('login')
-    async loginUser(@Body() loginUser: UserLoginDto): Promise<UserLoginDtoAuth | UserLoginDtoError> {
-        return await this.authService.loginService(loginUser);
-    }
+  @ApiOperation({ summary: 'User authentication' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: UserLoginDtoAuth,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Error',
+    type: UserLoginDtoError,
+  })
+  @Post('login')
+  async loginUser(
+    @Body() loginUser: UserLoginDto,
+  ): Promise<UserLoginDtoAuth | UserLoginDtoError> {
+    return await this.authService.loginService(loginUser);
+  }
 
-    @UseGuards(AuthGuard)
-    @ApiOperation({ summary: 'Refresh token' })
-    @ApiResponse({
-        status: HttpStatus.OK,
-        description: "Success",
-        type: UserUpdateTokenDto
-    })
-    @ApiResponse({
-        status: HttpStatus.UNAUTHORIZED,
-        description: "Error",
-        type: UserLoginDtoError
-    })
-    @Post('refresh')
-    async refresh(@Body() currentToken: RefreshTokenDto): Promise<UserUpdateTokenDto | UserLoginDtoError> {
-        return await this.authService.refreshService(currentToken);
-    }
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Refresh token' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: UserUpdateTokenDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Error',
+    type: UserLoginDtoError,
+  })
+  @Post('refresh')
+  async refresh(
+    @Body() currentToken: RefreshTokenDto,
+  ): Promise<UserUpdateTokenDto | UserLoginDtoError> {
+    return await this.authService.refreshService(currentToken);
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Get('forgot')
-    async forgot(@Query('email') email: string) {
-        return await this.authService.forgotService(email);
-    }
+  @HttpCode(HttpStatus.OK)
+  @Get('forgot')
+  async forgot(@Query('email') email: string) {
+    return await this.authService.forgotService(email);
+  }
 }
